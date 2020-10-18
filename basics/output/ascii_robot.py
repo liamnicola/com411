61945c9cb03a806482837bc6601ec2ffd7821ef0
repl.it user <replituser@example.com>
2020-10-18 19:10:34 +0000

    ascii robot

---
+++ basics/output/ascii_robot.py
@@ -0,0 +1,13 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+e = input("please enter a character")
+
+print("##########")
+print("#",(e),  (e)," #")
+print("#  ----  #")
+print("##########")
+
---
+++ basics/output/data_types.py
@@ -0,0 +1,3 @@
+name = (str(input("What is your name?")))
+
+age = (int(input("How old are you (in years)?")))
