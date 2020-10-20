c6375f41bb38a619412898ed91197b61f3f6bce3
repl.it user <replituser@example.com>
2020-10-20 10:17:16 +0000

    simple list

---
+++ data/lists/simple_list.py
@@ -0,0 +1,10 @@
+def direction ():
+  direction = []
+
+  direction.append("Move Forward")
+  direction.append("Move Backwards")
+  direction.append("Turn Left")
+  direction.append("Turn Right")
+direction()
+
+print(direction)
