5678a649a344a962f0ff1081d174000c788d5f7d
repl.it user <replituser@example.com>
2020-10-27 10:40:43 +0000

    data sets

---
+++ data/sets/simple_set.py
@@ -0,0 +1,21 @@
+def observed():
+  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+  return observations
+
+def run():
+  observations = observed()
+  print(observations)
+print (run())
+  
+
+
+
+
+
+
+
+
+
+
+
+
